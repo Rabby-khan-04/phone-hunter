1ec44fd87780b03ac34ad23aa37d1c551f5323ea
Rabby Khan <ajrabbyK72@gmail.com>
2023-02-26 00:08:27 +0600

    Added Show All Button

--- app.js
+++ app.js
@@ -7,22 +7,29 @@
 
 // Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
 
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   try {
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
   } catch (error) {
     console.log(error);
   }
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, dataLimit) => {
   console.log(phones);
   const phonesContainer = document.getElementById("phone-container");
   phonesContainer.innerHTML = "";
-  // phones = phones.slice(0, 9);
+
+  const showAll = document.getElementById("show-all");
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 9);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
 
   const noPhoneMsg = document.getElementById("no-phone-message");
 
@@ -49,13 +56,16 @@ const displayPhones = (phones) => {
   toggleSpinner(false);
 };
 
-document.getElementById("search-btn").addEventListener("click", () => {
+const processAndDisplay = (dataLimit) => {
   toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
 
-  loadPhones(searchText);
-  searchField.value = "";
+  loadPhones(searchText, dataLimit);
+};
+
+document.getElementById("search-btn").addEventListener("click", () => {
+  processAndDisplay(10);
 });
 
 const toggleSpinner = (isLoading) => {
@@ -66,5 +76,10 @@ const toggleSpinner = (isLoading) => {
     spinner.classList.add("d-none");
   }
 };
+
+document.getElementById("view-all-btn").addEventListener("click", () => {
+  processAndDisplay();
+  document.getElementById("vshow-all").classList.add("d-none");
+});
 
 // loadPhones("samsung");
